6e716a867751237ef53a63bd7cbbc1d287f76340
Todd Albert <todd@bocacode.com>
2021-07-06 16:36:07 -0400

    connected to pet boutique database

---
+++ index.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql')
+const { dbconfig } = require('./dbconfig')
+const db = mysql.createConnection(dbconfig)
+db.connect()
+db.query('SELECT * FROM orders', (err, rows) => {
+  if(err) {
+    console.error('Error from MySQL: ', err)
+  }
+  console.log('Got back ROWS: ', rows)
+})
+db.end()
